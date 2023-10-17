d6a770f6b00efe407e6bea3e3984a9b4da93beae
Vedank Pande <vedank.pande@gmail.com>
2023-10-17 16:33:40 +0530

    add validation for post requests

--- src/rest/todo/views.py
+++ src/rest/todo/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .serializer import TaskSerializer
 from utils.database import get_db_connection
 from utils.json_encoder import JSONEncoder
 import json
@@ -19,14 +20,27 @@ class TodoListView(APIView):
             todo_objects = [json.dumps(item, cls=JSONEncoder)
                             for item in todo_items]
 
-            print("latest todo", todo_objects)
-            return Response(todo_objects, status=status.HTTP_200_OK)
+            return Response(data=todo_objects, status=status.HTTP_200_OK)
+
         except Exception as e:
-            return Response(data={"message": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            return Response(data={"error": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request):
 
-        print(request.body.decode('utf-8'))
-        #self.todo.insert_one()
-        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        serializer = TaskSerializer(data=request.data)
+        try:
+
+            if serializer.is_valid():
+
+                validated_data = serializer.validated_data
+                document = {"task": validated_data["task"]}
+                self.todo.insert_one(document)
+
+                return Response(data= json.dumps(document, cls=JSONEncoder), status=status.HTTP_201_CREATED)
+
+            else:
+
+                return Response(data={"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response(data={"error": e}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
