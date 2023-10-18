0d9e6e39ef7a7d1226e73ca9ccf40634f8c19072
Vedank Pande <vedank.pande@gmail.com>
2023-10-18 13:39:19 +0530

    clean up create task code

--- src/app/src/components/createTask.js
+++ src/app/src/components/createTask.js
@@ -1,32 +1,30 @@
 import { useState } from "react";
-import axios from "axios";
+
+import { createTask } from "../services/APIService";
+import CreateTaskForm from "./createTaskForm";
 
 export default function CreateTask(props) {
-    const [task, setTask] = useState("")
+  const [task, setTask] = useState("");
 
-    const handleSubmit = async (e)=>{
-        e.preventDefault()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        axios.post("http://localhost:8000/todos/",{
-            task: task
-        }).then((response)=>{
-            setTask("")
-            props.refreshCallback()
-        }).catch((error)=>{
-            alert(error.message)
-            setTask("")
-        })
-        
-    }
-    return (
-    <form onSubmit={handleSubmit}>
-        <input 
-        type="text"
-        placeholder="Enter task to create"
-        value={task}
-        onChange={(e)=> setTask(e.target.value)}
-        />
-        <button type="submit">Create Item</button>
-    </form>
-    );
+    createTask({
+      task: task,
+      refreshCallback: props.refreshCallback,
+      setTaskCallback: setTask,
+    });
+
+  };
+  
+  return (
+    <div>
+      <h1>Create a todo</h1>
+      <CreateTaskForm
+        task={task}
+        handleSubmitCallback={handleSubmit}
+        setTaskCallback={setTask}
+      />
+    </div>
+  );
 }
