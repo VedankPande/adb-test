c56465262f3a29d1d2c5485cd2f7d2358777b407
Vedank Pande <vedank.pande@gmail.com>
2023-10-19 11:58:46 +0530

    add validators module

---
+++ src/rest/utils/validators.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+
+
+class ValidationException(Exception):
+    pass
+
+
+class Validator(ABC):
+
+    @abstractmethod
+    def is_valid(self):
+        pass
+
+    @property
+    @abstractmethod
+    def validated_data(self):
+        pass
+
+    @property
+    @abstractmethod
+    def validated_data(self):
+        pass
+
+
+class TodoCreateValidator(Validator):
+    def __init__(self, data):
+        self.data = data
+        self._validated_data = None
+        self._error = None
+
+    def is_valid(self):
+
+        task = self.data.get("task", "")
+
+        # TODO: better checks?
+        if not task:
+            self.error = "Task is not present"
+            return False
+        self.validated_data = {"task": task}
+        return True
+
+    @property
+    def validated_data(self):
+        if not self._validated_data:
+            raise ValidationException(
+                "Data hasn't been validated yet, call 'is_valid' before accessing validated_data")
+        return self._validated_data
+
+    @validated_data.setter
+    def validated_data(self, data):
+        self._validated_data = data
+
+    @property
+    def error(self):
+        return self._error
+
+    @error.setter
+    def error(self, err):
+        self._error = err
